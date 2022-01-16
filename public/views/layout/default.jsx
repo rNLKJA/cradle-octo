9c78af61e2388e7e3dd09a239a6cdd414b62b132
Rin <62606765+chuangyu-hscy@users.noreply.github.com>
2022-01-16 14:47:19 +1100

    Update default.jsx

--- public/views/layout/default.jsx
+++ public/views/layout/default.jsx
@@ -4,9 +4,27 @@ function DefaultLayout(props) {
   return (
     <html>
       <head>
+        <meta name="author" content="rNLKJA" />
+        <meta charset="UTF-8" />
+        <meta name="description" content="Free Web tutorials" />
+        <meta name="keywords" content="HTML, CSS, JavaScript" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
         <title>{props.title}</title>
+
+        <link rel="stylesheet" href="./css/layout/defaultLayout.css" />
       </head>
       <body>{props.children}</body>
+      <footer>
+        @
+        <a
+          href="https://github.com/chuangyu-hscy"
+          alt="author github page"
+          className=""
+        >
+          rNLKJA
+        </a>
+      </footer>
     </html>
   );
 }
