7c4299f997eb8cae4e8cd6018f417ee6d13c760b
Rin <62606765+chuangyu-hscy@users.noreply.github.com>
2022-01-19 18:06:07 +1100

    Update appStartInfo.ts

--- data/appStartInfo.ts
+++ data/appStartInfo.ts
@@ -1,13 +1,14 @@
 require("dotenv").config();
 
 const logAppStartInformation = () => {
-  console.log("=======================================");
-  console.log("Project start    ：随便の后端技术堆积站");
+  console.log(
+    "======================================================================",
+  );
+  console.log("Project Cradle start：");
   console.log(`Listening at PORT：${process.env.PORT || 3000}`);
-  console.log("---------------------------------------");
-  console.log("开始运行：随便の后端技术堆积站");
-  console.log(`端口    ：${process.env.PORT || 3000}`);
-  console.log("=======================================");
+  console.log(
+    "======================================================================",
+  );
 };
 
 module.exports = logAppStartInformation;
