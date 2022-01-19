11efc2cd0751754478e028006231cef8821a7448
Rin <62606765+chuangyu-hscy@users.noreply.github.com>
2022-01-19 21:23:39 +1100

    Create randomGenerator.ts

---
+++ util/randomGenerator.ts
@@ -0,0 +1,17 @@
+/**
+ * Generate a random string
+ * This function will generate a random string with a default string length 8
+ * @param {int} length
+ */
+const randomStringGenerator = async (length = 8) => {
+  var result = "";
+  var characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~!@#$%^&*()_+=-";
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+};
+
+module.exports = { randomStringGenerator };
