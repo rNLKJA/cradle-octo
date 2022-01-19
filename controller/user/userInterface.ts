be2aab0f88235d0f719197d0a8b95002649160ba
Rin <62606765+chuangyu-hscy@users.noreply.github.com>
2022-01-19 20:03:27 +1100

    Create userInterface.ts

---
+++ controller/user/userInterface.ts
@@ -0,0 +1,11 @@
+// define custom interface
+export interface userType {
+  username: String;
+  password: String;
+  email: String;
+  phone: String;
+  occupation: String;
+  isDeveloper: Boolean;
+  isValid: Boolean;
+  isAdmin: Boolean;
+}
