e62c74931d867b8b10072ca465bb8923bcbd6fb7
Rin <62606765+chuangyu-hscy@users.noreply.github.com>
2022-01-22 01:19:09 +1100

    Update cradle.status.code.ts

--- data/cradle.status.code/cradle.status.code.ts
+++ data/cradle.status.code/cradle.status.code.ts
@@ -1,4 +1,7 @@
+// import required status code json file
 const loginStatusCode = require("./login.status.code.json");
+const userStatusCode = require("./user.status.code.json");
+const invitationStatusCode = require("./invitation.status.code.json");
 
 interface statusCodeInterface {
   status: boolean;
@@ -8,19 +11,39 @@ interface statusCodeInterface {
 
 /**
  * This function will search the status code from
- * the pre-defined status code json file
+ * the pre-defined status code json file for login related functions
  * @param {Number} statusCode
  * @return {statusCodeInterface} object
  */
 const login = (statusCode: Number) => {
   return loginStatusCode.filter(
     (code: statusCodeInterface) => code.csc === statusCode,
-  );
+  )[0];
 };
 
-const user = (statusCode: Number) => {};
+/**
+ * This function will search the status code from
+ * the pre-defined status code json file for user related functions
+ * @param {Number} statusCode
+ * @return {statusCodeInterface} object
+ */
+const user = (statusCode: Number) => {
+  return userStatusCode.filter(
+    (code: statusCodeInterface) => code.csc === statusCode,
+  )[0];
+};
 
-const invitation = (statusCode: Number) => {};
+/**
+ * This function will search the status code from
+ * the pre-defined status code json file for user related functions
+ * @param {Number} statusCode
+ * @return {statusCodeInterface} object
+ */
+const invitation = (statusCode: Number) => {
+  return invitationStatusCode.filter(
+    (code: statusCodeInterface) => code.csc === statusCode,
+  )[0];
+};
 
 module.exports = {
   login,
