a1971b5bb95ba48d278824979dada2f22e1695f4
Rin <62606765+chuangyu-hscy@users.noreply.github.com>
2022-01-21 21:47:45 +1100

    Update app.ts

--- app.ts
+++ app.ts
@@ -15,20 +15,20 @@ const bodyParser = require("body-parser");
 
 // define express application entry point
 const express = require("express");
-const app = express();
+const cradle = express();
 
 // set middleware
-app.set("views", path.resolve(__dirname + "/public/views"));
-app.set("view engine", "jsx");
-app.engine("jsx", require("express-react-views").createEngine());
-app.use(express.static("public"));
-app.use(bodyParser.json()); // enable json request parse ability
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(cors(corsConfig));
+cradle.set("views", path.resolve(__dirname + "/public/views"));
+cradle.set("view engine", "jsx");
+cradle.engine("jsx", require("express-react-views").createEngine());
+cradle.use(express.static("public"));
+cradle.use(bodyParser.json()); // enable json request parse ability
+cradle.use(bodyParser.urlencoded({ extended: true }));
+cradle.use(session({ name: "jwt", keys: ["ProjectCradle"] }));
+cradle.use(cors(corsConfig));
 
 // define back-end application home route
-app.get("/", (req: Request, res: Response) => {
+cradle.get("/", (req: Request, res: Response) => {
   // res.sendFile(path.resolve(`${__dirname}/public/views/index.html`));
   res.render("./index/index", { name: "rNLKJA" });
 });
@@ -36,14 +36,14 @@ app.get("/", (req: Request, res: Response) => {
 // import custom routers
 const userRouter = require("./routes/userRouter");
 
-app.use("/user", userRouter);
+cradle.use("/user", userRouter);
 
 // define 404 response page
-app.get("*", (req: Request, res: Response) => {
+cradle.get("*", (req: Request, res: Response) => {
   res.render("./error/404");
 });
 
 // start back-end server
-app.listen(process.env.PORT || 3000, require("./data/appStartInfo")());
+cradle.listen(process.env.PORT || 3000, require("./data/appStartInfo")());
 
-module.exports = app;
+module.exports = cradle;
